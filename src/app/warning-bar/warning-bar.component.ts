f3eaeb6b1d7af7734b825b816e7542a9e1f9e83a
PhilippeBel <philippebelanger97@Gmail.com>
2020-01-29 14:12:17 -0500

    added x button on warning bar

--- src/app/warning-bar/warning-bar.component.ts
+++ src/app/warning-bar/warning-bar.component.ts
@@ -7,36 +7,54 @@ import { Observable, timer } from 'rxjs';
   styleUrls: ['./warning-bar.component.scss']
 })
 
-
-
 export class WarningBarComponent implements OnInit {
 
-  readonly INIT_TOP = -48;
+  initTop: number;
   title: string;
   frame: number;
   top: number;
   height: string;
   everyFrame: Observable<number> = timer(0,33);
+  visi: string;
+  closing: boolean;
 
-  constructor() { }
+  constructor() {
+    this.initTop = -48;
+    this.closing = false;
+    this.top = this.initTop;
+    this.frame = 0;
+    this.height = this.frame.toString() + "px";
+    this.visi = "hidden"; }
 
   ngOnInit() {
     this.title = "Winter storm warning";
-    this.top = this.INIT_TOP;
-    this.frame = 0;
-    this.height = this.frame.toString() + "px";
+
 
     this.everyFrame.subscribe((seconds) => this.Update());
   }
 
   Update(){
     this.frame++;
-    if(this.top < 0){
-    this.top = (this.INIT_TOP + this.frame * 2);
-    this.height = this.top.toString() + "px";
+    
+    if(this.closing){
+      if(this.top < -64)
+      this.visi = "hidden"
+      else
+      this.top = (this.initTop - this.frame * 4);
+    } else if(this.top < 0){
+    this.visi = "visible";
+    this.top = (this.initTop + this.frame * 4);
     }
+    
+    this.height = this.top.toString() + "px";
   }
 
-  
+  onClick() {
+    if(!this.closing){
+    this.closing = true;
+    this.initTop = this.top;
+    this.frame = 0;
+    }
+  }
 
 }
