60ec476ba886e836f3785c3ae98ebabd07a58414
PhilippeBel <philippebelanger97@Gmail.com>
2020-02-11 16:23:58 -0500

    added mocks for cities and provinces

---
+++ src/mocks/city-mocks.ts
@@ -0,0 +1,8 @@
+const CITIES_MOCK: City[] = [
+    {id: 0, name: "Montréal", province: PROVINCES_MOCK[0]},
+    {id: 1, name: "Québec", province: PROVINCES_MOCK[0]},
+    {id: 2, name: "Toronto", province: PROVINCES_MOCK[1]},
+    {id: 3, name: "Ottawa", province: PROVINCES_MOCK[1]},
+    {id: 4, name: "Vancouver", province: PROVINCES_MOCK[2]},
+    {id: 5, name: "Calgary", province: PROVINCES_MOCK[3]},
+];
---
+++ src/mocks/province-mocks.ts
@@ -0,0 +1,6 @@
+const PROVINCES_MOCK: Province[] = [
+    {id: 0, name: "Québec", abreviation: "QC"},
+    {id: 1, name: "Ontario", abreviation: "ON"},
+    {id: 2, name: "British Columbia", abreviation: "BC"},
+    {id: 3, name: "Alberta", abreviation: "AB"},
+];
