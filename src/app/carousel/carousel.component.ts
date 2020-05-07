fa5c44f3bee957a8f398fa460286b0f7d0a4a01c
PhilippeBel <philippebelanger97@Gmail.com>
2020-05-07 15:19:06 -0400

    implemented slide animation

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { News } from 'src/models/news';
 
+const SLIDE_SPEED = 7;
+
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
@@ -12,18 +14,39 @@ export class CarouselComponent implements OnInit {
   newsDisplayed: News[] = new Array<News>();
   slideCounter: number;
   maxSlides: number;
+  offsetX: number;
+  
 
   constructor() { }
 
   ngOnInit() {
     this.slideCounter = 1;
+    this.offsetX = 0;
     this.maxSlides = this.newsList.length - 1;
     this.newsDisplayed.push(this.newsList[0]);
     this.newsDisplayed.push(this.newsList[1]);
+    setInterval(() =>{
+      this.updateFrame();
+    },17);
+  }
+
+  updateFrame(){
+    if(this.offsetX > 0)
+    {
+      this.offsetX -= SLIDE_SPEED;
+      if(this.offsetX < SLIDE_SPEED)
+        this.offsetX = 0;
+    }
+    else if(this.offsetX < 0){
+      this.offsetX += SLIDE_SPEED;
+      if(this.offsetX > SLIDE_SPEED)
+        this.offsetX = 0;
+    }
   }
 
   nextSlide(){
     if(this.slideCounter < this.maxSlides){
+      this.offsetX = 50;
       this.slideCounter++;
       this.newsDisplayed.shift();
       this.newsDisplayed.push(this.newsList[this.slideCounter]);
@@ -32,6 +55,7 @@ export class CarouselComponent implements OnInit {
 
   previousSlide(){
     if(this.slideCounter > 1){
+      this.offsetX = -50;
       this.slideCounter--;
       this.newsDisplayed.pop();
       this.newsDisplayed.unshift(this.newsList[this.slideCounter - 1]);
--- src/app/news-page/news-page.component.ts
+++ src/app/news-page/news-page.component.ts
@@ -21,5 +21,4 @@ export class NewsPageComponent implements OnInit {
       }
     );
   }
-
 }
